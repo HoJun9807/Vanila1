f36d901817562e7cee5f2abea7dee55ed4942dac
임호준 <ghwns5087@naver.com>
2022-10-05 21:52:53 +0900

    Add files via upload

---
+++ momentum/app.js
@@ -0,0 +1,76 @@
+// const a = 5;
+// let b = 3;
+
+// console.log(a+b); //8
+
+// b = 5;
+
+// console.log(a+b); //10
+
+// const weekend = ["mon","tue","wed","thu","fri","sat","sun"];
+// console.log(weekend);
+// //Get Item from Array
+// console.log(weekend[1]);
+// console.log(weekend.length);
+// //Add one more day to the array
+// weekend.push("haha");
+// console.log(weekend);
+
+// const me = {
+//     name : " hojun",
+//     points : 10,
+//     hight : 175,
+// };
+
+// console.log(me);
+// console.log(me.hight); //175
+// console.log(me["points"]); //10
+// me.points = 20; 
+// console.log(me["points"]); //20
+// //Add one more day to the object
+// me.fristName = "lim";
+// console.log(me);
+// //data update
+// me.points = me.points + 5;
+// console.log(me["points"]); //25
+
+
+// function a(a,b){ //argument a , b
+//     console.log(a+b);
+// }
+// a(10,10);//20
+
+// const calculater = {
+//     plus: function(a,b){
+//         console.log(a+b);
+//     },
+//     mius: function(a,b){
+//         console.log(a-b);
+//     },
+//     gub: function(a,b){
+//         console.log(a*b);
+//     },
+//     div: function(a,b){
+//         console.log(a/b);
+//     },
+// };
+// calculater.plus(10,4); //14
+// calculater.mius(10,4); //6
+// calculater.gub(10,4); //40
+// calculater.div(10,4); //2.5
+
+// const age = 98;
+// function calculaterKrAge(ageOfForeigner){
+//     return ageOfForeigner + 2;
+// }
+
+// const krAge = calculaterKrAge(age);
+// console.log(krAge);
+
+const fofo = {
+    ak : function(a){
+        return a
+    }
+}
+
+const qkqk = fofo.ak("zlzl");
