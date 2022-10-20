cf5a120216c4ef92eb69377ab64445a3e5bc0d82
임호준 <ghwns5087@naver.com>
2022-10-20 18:32:47 +0900

    Add files via upload

--- vanilajs/js/weather.js
+++ vanilajs/js/weather.js
@@ -1,8 +1,17 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "02727a7acd967b05c2c0a4b56542558c";
+
 function onGeoOK(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng)
-
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGeoError(){
     alert("Can't find you. No weather for yoy.");
@@ -10,4 +19,3 @@ function onGeoError(){
 
 navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
 
-//https://api.openweathermap.org/data/2.5/weather?lat=37.6641656&lon=127.0460668&appid=02727a7acd967b05c2c0a4b56542558c
